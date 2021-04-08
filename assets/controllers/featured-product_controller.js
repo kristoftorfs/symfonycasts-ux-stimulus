a9b6ea0d6d12ee5d36bec0a43b388c7aa7648c59
Kristof Torfs <kristof@torfs.org>
2021-04-08 15:30:20 +0200

    Finished chapter 27

---
+++ assets/controllers/featured-product_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from 'stimulus';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import FeaturedProduct from "../components/FeaturedProduct";
+
+export default class extends Controller {
+    static values = {
+        product: Object
+    }
+
+    connect() {
+        ReactDOM.render(
+            <FeaturedProduct product={this.productValue} />,
+            this.element
+        );
+    }
+}
